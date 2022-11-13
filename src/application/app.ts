0a782ce155290e7d476ac343ef2459e89d56881d
mibi <binhhm2009@gmail.com>
2022-11-13 13:53:37 +0700

    Fix image path of build

--- src/application/app.ts
+++ src/application/app.ts
@@ -2,7 +2,7 @@ import { atom } from 'nanostores';
 
 export const isMenuOpen = atom(false);
 
-export const activeMenu = atom(1);
+export const activeMenu = atom(0);
 
 export function setActive(index: number) {
   activeMenu.set(index);
